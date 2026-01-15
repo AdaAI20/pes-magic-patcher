62b47809acfb1be69893416b635d6d9f6b0b8060
AdaAI20 <sokr1114@gmail.com>
2026-01-15 22:54:11 +0200

    Create pesCrypto.js

---
+++ src/crypto/pesCrypto.js
@@ -0,0 +1,19 @@
+import Module from "../wasm/pes_crypto.js";
+
+let wasm;
+
+export async function initCrypto() {
+  wasm = await Module();
+}
+
+export function decryptBuffer(uint8Array, key = 0xAA) {
+  const ptr = wasm._malloc(uint8Array.length);
+  wasm.HEAPU8.set(uint8Array, ptr);
+
+  wasm._decrypt(ptr, uint8Array.length, key);
+
+  const out = wasm.HEAPU8.slice(ptr, ptr + uint8Array.length);
+  wasm._free(ptr);
+
+  return out;
+}
