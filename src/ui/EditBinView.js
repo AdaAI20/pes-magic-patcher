7d3b2d74be7e747e9f8a3333a1dc1d2873ab0559
AdaAI20 <sokr1114@gmail.com>
2026-01-15 22:56:43 +0200

    Create EditBinView.js

---
+++ src/ui/EditBinView.js
@@ -0,0 +1,13 @@
+import { loadEditBin, exportEditBin } from "../parsers/editBinParser.js";
+
+export function createEditBinUI(container) {
+  const input = document.createElement("input");
+  input.type = "file";
+
+  input.onchange = async () => {
+    const edit = await loadEditBin(input.files[0]);
+    console.log("EDIT loaded", edit);
+  };
+
+  container.appendChild(input);
+}
