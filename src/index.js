df97f7b40172f0b019f2f400115d25cf9b0c06b5
AdaAI20 <sokr1114@gmail.com>
2026-01-15 23:22:51 +0200

    Delete src/index.js

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import { initCrypto } from "./crypto/pesCrypto.js";
-import { createEditBinUI } from "./ui/EditBinView.js";
-import { createOptionUI } from "./ui/OptionFileView.js";
-
-await initCrypto();
-
-createEditBinUI(document.getElementById("edit-bin"));
-createOptionUI(document.getElementById("option-file"));
