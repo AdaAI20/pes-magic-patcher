7e86743dac7e24d058bdd84403ecf069e4609600
AdaAI20 <sokr1114@gmail.com>
2026-01-16 01:01:34 +0200

    Create keys.js

---
+++ src/crypto/keys.js
@@ -0,0 +1,20 @@
+// PES 2021 known edit.bin seed
+export const PES_EDIT_KEY = new Uint8Array([
+  0x45, // E
+  0x44, // D
+  0x49, // I
+  0x54, // T
+  0x42, // B
+  0x49, // I
+  0x4E  // N
+]);
+
+// Option file seed (placeholder – will expand later)
+export const PES_OPTION_KEY = new Uint8Array([
+  0x4F, // O
+  0x50, // P
+  0x54, // T
+  0x49, // I
+  0x4F, // O
+  0x4E  // N
+]);
