681ccefd7a0c279857d3dbf9a26bffe55781cb85
AdaAI20 <sokr1114@gmail.com>
2026-01-16 04:15:20 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,18 @@
-import { Routes, Route, NavLink } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import OptionFiles from "./pages/OptionFiles";
-import Edit00000000 from "./pages/Edit00000000";
-import NotFound from "./pages/NotFound";
-import Teams from "./pages/Teams";
-import Players from "./pages/Players";
+import EditBin from "./pages/EditBin";
+import OptionFile from "./pages/OptionFile";
 
 export default function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-100">
-      <header className="bg-white shadow-md p-4 flex gap-4">
-        <NavLink to="/" className="font-bold hover:underline">
-          Home
-        </NavLink>
-        <NavLink to="/option-files" className="hover:underline">
-          Option Files
-        </NavLink>
-        <NavLink to="/edit00000000" className="hover:underline">
-          EDIT00000000
-        </NavLink>
-        <NavLink to="/teams" className="hover:underline">
-          Teams
-        </NavLink>
-        <NavLink to="/players" className="hover:underline">
-          Players
-        </NavLink>
-      </header>
-
-      <main className="flex-1 p-6">
+    <div className="min-h-screen bg-background text-foreground">
+      <BrowserRouter basename="/pes-magic-patcher">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/option-files" element={<OptionFiles />} />
-          <Route path="/edit00000000" element={<Edit00000000 />} />
-          <Route path="/teams" element={<Teams />} />
-          <Route path="/players" element={<Players />} />
-
-          <Route path="*" element={<NotFound />} />
+          <Route path="/edit-bin" element={<EditBin />} />
+          <Route path="/option-file" element={<OptionFile />} />
         </Routes>
-      </main>
+      </BrowserRouter>
     </div>
   );
 }
