980fa7f312239d02cfc0c8b8ffa4061ddf7900c7
AdaAI20 <sokr1114@gmail.com>
2026-01-16 02:31:30 +0200

    Update editBinParser.js

--- src/parsers/editBinParser.js
+++ src/parsers/editBinParser.js
@@ -1,11 +1,16 @@
-import { decryptEditBin } from "@/crypto/pesCrypto";
+import { decryptEditBin } from "../crypto/pesCrypto";
 
 export async function loadEditBin(file) {
-  const encrypted = new Uint8Array(await file.arrayBuffer());
-  const decrypted = decryptEditBin(encrypted);
+  const buffer = await file.arrayBuffer();
+  const decrypted = decryptEditBin(buffer);
 
   return {
     raw: decrypted,
-    size: decrypted.length
+    size: decrypted.byteLength
   };
+}
+
+export function exportEditBin(data) {
+  // For now, export raw buffer (encryption comes later in STEP A2/A3)
+  return new Blob([data], { type: "application/octet-stream" });
 }
