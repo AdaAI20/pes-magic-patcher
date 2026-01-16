e03946ad42b3703bea01c3222813062c4b37020a
AdaAI20 <sokr1114@gmail.com>
2026-01-16 03:04:11 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,22 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { MainLayout } from "@/components/layout/MainLayout";
-
-import Dashboard from "./pages/Dashboard";
-import Players from "./pages/Players";
-import Teams from "./pages/Teams";
-import Leagues from "./pages/Leagues";
-import Faces from "./pages/Faces";
-import Kits from "./pages/Kits";
-import Balls from "./pages/Balls";
-import Import from "./pages/Import";
-import Export from "./pages/Export";
-import PatchBuilder from "./pages/PatchBuilder";
-import FileBrowser from "./pages/FileBrowser";
-import SettingsPage from "./pages/Settings";
-import NotFound from "./pages/NotFound";
-
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
 import EditBin from "./pages/EditBin";
-import OptionFiles from "./pages/OptionFiles";
+import OptionFile from "./pages/OptionFile";
 
-const queryClient = new QueryClient();
+export default function App() {
+  return (
+    <div className="min-h-screen p-6">
+      <nav className="flex gap-4 mb-6">
+        <Link to="/" className="underline">Home</Link>
+        <Link to="/edit-bin" className="underline">EDIT.BIN</Link>
+        <Link to="/option-file" className="underline">Option File</Link>
+      </nav>
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <MainLayout>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/players" element={<Players />} />
-            <Route path="/teams" element={<Teams />} />
-            <Route path="/leagues" element={<Leagues />} />
-            <Route path="/faces" element={<Faces />} />
-            <Route path="/kits" element={<Kits />} />
-            <Route path="/balls" element={<Balls />} />
-
-            {/* NEW */}
-            <Route path="/edit-bin" element={<EditBin />} />
-            <Route path="/option-files" element={<OptionFiles />} />
-
-            <Route path="/import" element={<Import />} />
-            <Route path="/export" element={<Export />} />
-            <Route path="/patch-builder" element={<PatchBuilder />} />
-            <Route path="/file-browser" element={<FileBrowser />} />
-            <Route path="/settings" element={<SettingsPage />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </MainLayout>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/edit-bin" element={<EditBin />} />
+        <Route path="/option-file" element={<OptionFile />} />
+      </Routes>
+    </div>
+  );
+}
