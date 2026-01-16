ae2b362132249b385bea83e531b466bcc56e5051
AdaAI20 <sokr1114@gmail.com>
2026-01-16 19:23:07 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "@/components/layout/MainLayout";
 
-import Dashboard from "./pages/Dashboard";
-import Players from "./pages/Players";
-import Teams from "./pages/Teams";
-import Leagues from "./pages/Leagues";
-import Import from "./pages/Import";
-import EditBin from "./pages/EditBin";
-
-import MainLayout from "./components/layout/MainLayout";
+import Dashboard from "@/pages/Dashboard";
+import Players from "@/pages/Players";
+import Teams from "@/pages/Teams";
+import Leagues from "@/pages/Leagues";
+import Faces from "@/pages/Faces";
+import Kits from "@/pages/Kits";
+import Balls from "@/pages/Balls";
+import EditBin from "@/pages/EditBin";
+import ExportPage from "@/pages/Export";
+import PatchBuilder from "@/pages/PatchBuilder";
+import FileBrowser from "@/pages/FileBrowser";
+import Settings from "@/pages/Settings";
 
 export default function App() {
   return (
@@ -18,8 +23,16 @@ export default function App() {
           <Route path="/players" element={<Players />} />
           <Route path="/teams" element={<Teams />} />
           <Route path="/leagues" element={<Leagues />} />
-          <Route path="/import" element={<Import />} />
-          <Route path="/edit-bin" element={<EditBin />} />
+
+          <Route path="/faces" element={<Faces />} />
+          <Route path="/kits" element={<Kits />} />
+          <Route path="/balls" element={<Balls />} />
+
+          <Route path="/import" element={<EditBin />} />
+          <Route path="/export" element={<ExportPage />} />
+          <Route path="/patch-builder" element={<PatchBuilder />} />
+          <Route path="/file-browser" element={<FileBrowser />} />
+          <Route path="/settings" element={<Settings />} />
         </Route>
       </Routes>
     </HashRouter>
