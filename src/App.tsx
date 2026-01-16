7ff04759b54cbe19199b905ad0e72cdfc71d69bf
AdaAI20 <sokr1114@gmail.com>
2026-01-16 05:18:08 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
-import { MainLayout } from "@/components/layout/MainLayout";
 
-import Dashboard from "@/pages/Dashboard";
-import Import from "@/pages/Import";
-import EditBin from "@/pages/EditBin";
-import NotFound from "@/pages/NotFound";
+import Dashboard from "./pages/Dashboard";
+import Players from "./pages/Players";
+import Teams from "./pages/Teams";
+import Leagues from "./pages/Leagues";
+import Import from "./pages/Import";
+import EditBin from "./pages/EditBin";
+
+import MainLayout from "./components/layout/MainLayout";
 
 export default function App() {
   return (
@@ -12,9 +15,11 @@ export default function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/players" element={<Players />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/leagues" element={<Leagues />} />
           <Route path="/import" element={<Import />} />
           <Route path="/edit-bin" element={<EditBin />} />
-          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </HashRouter>
