c3c2bc1490022330205b02d29ffe429e307a9daa
AdaAI20 <sokr1114@gmail.com>
2026-01-16 03:24:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import EditBin from "./pages/EditBin";
 import OptionFile from "./pages/OptionFile";
+import { Button } from "@/components/ui/button";
 
 export default function App() {
   return (
-    <div className="min-h-screen p-6">
-      <nav className="flex gap-4 mb-6">
-        <Link to="/" className="underline">Home</Link>
-        <Link to="/edit-bin" className="underline">EDIT.BIN</Link>
-        <Link to="/option-file" className="underline">Option File</Link>
-      </nav>
+    <div className="min-h-screen flex flex-col">
+      {/* Header */}
+      <header className="border-b p-4 flex gap-3">
+        <Button asChild variant="outline">
+          <a href="#/">Home</a>
+        </Button>
+        <Button asChild variant="outline">
+          <a href="#/edit-bin">EDIT.BIN</a>
+        </Button>
+        <Button asChild variant="outline">
+          <a href="#/option-file">Option File</a>
+        </Button>
+      </header>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/edit-bin" element={<EditBin />} />
-        <Route path="/option-file" element={<OptionFile />} />
-      </Routes>
+      {/* Page content */}
+      <main className="flex-1 p-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/edit-bin" element={<EditBin />} />
+          <Route path="/option-file" element={<OptionFile />} />
+        </Routes>
+      </main>
     </div>
   );
 }
