aa48dc7d3075a6e921f20787a3e7b90794c1d6b1
AdaAI20 <sokr1114@gmail.com>
2026-01-16 03:05:07 +0200

    Create Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">
+        PES 2021 Magic Patcher
+      </h1>
+      <p>Select a tool from the navigation above.</p>
+    </div>
+  );
+}
