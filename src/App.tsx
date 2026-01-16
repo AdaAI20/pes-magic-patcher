5259d82bb8d92ad144b1f4736074a69c8176dee0
AdaAI20 <sokr1114@gmail.com>
2026-01-16 05:05:18 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import EditBin from "./pages/EditBin";
 import OptionFile from "./pages/OptionFile";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/pes-magic-patcher">
-      <div className="min-h-screen bg-background text-foreground">
+    <div className="min-h-screen bg-background text-foreground">
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/import" element={<OptionFile />} />
           <Route path="/edit-bin" element={<EditBin />} />
-          <Route path="/option-file" element={<OptionFile />} />
         </Routes>
-      </div>
-    </BrowserRouter>
+      </HashRouter>
+    </div>
   );
 }
