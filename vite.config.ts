71da7701ec9d44b4faf2c2c4fce59baaec8058f9
AdaAI20 <sokr1114@gmail.com>
2026-01-16 03:57:28 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   base: "/pes-magic-patcher/",
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: "react",
+      jsxRuntime: "automatic",
+    }),
+  ],
 });
