b961ae53d26914afebeae6ff00537f77d474fb1d
AdaAI20 <sokr1114@gmail.com>
2026-01-16 03:34:59 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,51 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import EditBin from "./pages/EditBin";
-import OptionFile from "./pages/OptionFile";
-import { Button } from "@/components/ui/button";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MainLayout } from "@/components/layout/MainLayout";
+import Dashboard from "./pages/Dashboard";
+import Players from "./pages/Players";
+import Teams from "./pages/Teams";
+import Leagues from "./pages/Leagues";
+import Faces from "./pages/Faces";
+import Kits from "./pages/Kits";
+import Balls from "./pages/Balls";
+import Import from "./pages/Import";
+import Export from "./pages/Export";
+import PatchBuilder from "./pages/PatchBuilder";
+import FileBrowser from "./pages/FileBrowser";
+import SettingsPage from "./pages/Settings";
+import NotFound from "./pages/NotFound";
 
-export default function App() {
-  return (
-    <div className="min-h-screen flex flex-col">
-      {/* Header */}
-      <header className="border-b p-4 flex gap-3">
-        <Button asChild variant="outline">
-          <a href="#/">Home</a>
-        </Button>
-        <Button asChild variant="outline">
-          <a href="#/edit-bin">EDIT.BIN</a>
-        </Button>
-        <Button asChild variant="outline">
-          <a href="#/option-file">Option File</a>
-        </Button>
-      </header>
+const queryClient = new QueryClient();
 
-      {/* Page content */}
-      <main className="flex-1 p-6">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/edit-bin" element={<EditBin />} />
-          <Route path="/option-file" element={<OptionFile />} />
-        </Routes>
-      </main>
-    </div>
-  );
-}
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <MainLayout>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/players" element={<Players />} />
+            <Route path="/teams" element={<Teams />} />
+            <Route path="/leagues" element={<Leagues />} />
+            <Route path="/faces" element={<Faces />} />
+            <Route path="/kits" element={<Kits />} />
+            <Route path="/balls" element={<Balls />} />
+            <Route path="/import" element={<Import />} />
+            <Route path="/export" element={<Export />} />
+            <Route path="/patch-builder" element={<PatchBuilder />} />
+            <Route path="/file-browser" element={<FileBrowser />} />
+            <Route path="/settings" element={<SettingsPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </MainLayout>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
