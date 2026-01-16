e71af308010675b673ef9f9b6efc9fa73a9d7381
AdaAI20 <sokr1114@gmail.com>
2026-01-16 04:11:59 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
+    <ThemeProvider defaultTheme="dark" storageKey="pes-theme">
       <App />
-    </HashRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
